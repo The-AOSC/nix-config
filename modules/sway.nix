043c73a087849dfb5bdb9be87eff90e350d0eeea
The AOSC <the-aosc@tutamail.com>
2025-01-08 18:55:08 +0500

    Fix swaylock no longer works on qtile

--- modules/sway.nix
+++ modules/sway.nix
@@ -3,7 +3,14 @@
     userPackages = [];
   };
   programs.sway.enable = config.modules.lib.withModuleUsersConfig "sway" true;
-  programs.sway.extraPackages = config.modules.lib.withModuleUsersConfig "sway" (lib.lists.remove pkgs.foot options.programs.sway.extraPackages.default);
+  programs.sway.extraPackages = config.modules.lib.withModuleUsersConfig "sway" ((
+    lib.lists.remove pkgs.swaylock (
+      lib.lists.remove pkgs.foot options.programs.sway.extraPackages.default
+      )) ++ [(pkgs.swaylock.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          ../patches/swaylock/swaylock-1.8.0-revert-drop-support-for-layer-shell.patch
+        ];
+      }))]);
   services.libinput.enable = config.modules.lib.withModuleUsersConfig "sway" true;
   hardware.graphics.enable = config.modules.lib.withModuleUsersConfig "sway" true;
   # TODO: is this necessary?
