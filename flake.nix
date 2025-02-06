d72ea6165002b246f83315264f00b3b5b221d800
The AOSC <the-aosc@tutamail.com>
2025-02-06 15:57:06 +0500

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
   description = "NixOS configuration of The AOSC";
   inputs = {
-    nixpkgs.follows = "nixpkgs-unstable";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
     # https://flakehub.com/flake/AshleyYakeley/NixVirt
     nixvirt.url = "https://flakehub.com/f/AshleyYakeley/NixVirt/*.tar.gz";
     nixvirt.inputs.nixpkgs.follows = "nixpkgs";
-    nixvirt.inputs.nixpkgs-ovmf.follows = "nixpkgs-unstable";
+    nixvirt.inputs.nixpkgs-ovmf.follows = "nixpkgs";
   };
   outputs = inputs@{
     home-manager,
