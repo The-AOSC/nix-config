cc6419d74514ebee56d8c754e29d9b5f06adafc0
The AOSC <the-aosc@tutamail.com>
2025-03-06 10:03:31 +0500

    Force safe permissions on ~/.gnupg

--- home-modules/gpg/default.nix
+++ home-modules/gpg/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{config, pkgs, lib, ...}: {
   programs.gpg = {
     enable = true;
     mutableKeys = false;
@@ -18,6 +18,13 @@
       "8FEA1239D48FB852123025D0FCFA4475EDCF4912"  # gpg --list-keys --with-keygrip
     ];
     pinentryPackage = pkgs.pinentry-qt;
+  };
+  home.activation = {
+    force-private-gpg = lib.hm.dag.entryAfter ["writeBoundary" "createAndMountPersistentStoragePaths"] ''
+      run chmod -077 ${config.home.homeDirectory}/.gnupg
+      run chmod -077 /persist/home/aosc/.gnupg
+      run chmod -077 /persist/home/aosc/.gnupg/private-keys-v1.d
+    '';
   };
   home.persistence."/persist/home/aosc" = {
     directories = [
