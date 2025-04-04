1f802d4664485bf41cfdd2e4b7c3c04d7dc402b6
The AOSC <the-aosc@tutamail.com>
2025-04-04 10:43:16 +0500

    Export mkNixosSystem

--- flake.nix
+++ flake.nix
@@ -53,48 +53,51 @@
           else working-wine;
       };
     };
-    nixosConfigurations = builtins.mapAttrs (host-name: host-config: (
-      let
-        system = host-config.system;
-        specialArgs = {
-          inherit inputs;
-        };
-      in
-        nixpkgs.lib.nixosSystem {
-          inherit system specialArgs;
-          modules =
-            [
-              ({pkgs, ...}: {
-                environment.systemPackages = [pkgs.git];
-                networking.hostName = host-name;
-                nix.settings.experimental-features = ["nix-command" "flakes"];
-              })
-              {
-                nixpkgs.overlays = host-config.overlays;
-              }
-              home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users =
-                    builtins.mapAttrs (
-                      user-name: home-config: ({osConfig, ...}: {
-                        imports = home-config.modules;
-                        home.homeDirectory = osConfig.users.users."${user-name}".home;
-                        home.stateVersion = osConfig.system.stateVersion;
-                        home.username = user-name;
-                        programs.home-manager.enable = true;
-                      })
-                    )
-                    host-config.home;
-                  extraSpecialArgs = specialArgs;
-                };
-              }
-            ]
-            ++ host-config.nixos-modules;
-        }
-    )) (import ./hosts inputs);
+    nixosConfigurations = builtins.mapAttrs self.lib.mkNixosSystem (import ./hosts inputs);
+    lib = {
+      mkNixosSystem = host-name: host-config: (
+        let
+          system = host-config.system;
+          specialArgs = {
+            inherit inputs;
+          };
+        in
+          nixpkgs.lib.nixosSystem {
+            inherit system specialArgs;
+            modules =
+              [
+                ({pkgs, ...}: {
+                  environment.systemPackages = [pkgs.git];
+                  networking.hostName = host-name;
+                  nix.settings.experimental-features = ["nix-command" "flakes"];
+                })
+                {
+                  nixpkgs.overlays = host-config.overlays;
+                }
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users =
+                      builtins.mapAttrs (
+                        user-name: home-config: ({osConfig, ...}: {
+                          imports = home-config.modules;
+                          home.homeDirectory = osConfig.users.users."${user-name}".home;
+                          home.stateVersion = osConfig.system.stateVersion;
+                          home.username = user-name;
+                          programs.home-manager.enable = true;
+                        })
+                      )
+                      host-config.home;
+                    extraSpecialArgs = specialArgs;
+                  };
+                }
+              ]
+              ++ host-config.nixos-modules;
+          }
+      );
+    };
     formatter = flake-utils.lib.eachDefaultSystemMap (
       system: nixpkgs.legacyPackages.${system}.alejandra
     );
