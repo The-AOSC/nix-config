e5fc199b961d4515d1663cca50ee7c45dc1a3371
The AOSC <the-aosc@tutamail.com>
2025-08-29 21:54:13 +0500

    Update tmux configuration

--- home-modules/tmux/default.nix
+++ home-modules/tmux/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }: {
@@ -12,6 +13,30 @@
       clock24 = true;
       keyMode = "vi";
       newSession = true;
+      sensibleOnTop = true;
+      plugins = with pkgs.tmuxPlugins; [
+        {
+          plugin = battery;
+          extraConfig = ''
+            set -g status-right '${lib.concatStrings [
+              # pane title
+              "#{?window_bigger,"
+              "[#{window_offset_x}#,#{window_offset_y}] ,}"
+              "\"#{=21:pane_title}\"|"
+              # battery indicator
+              "#{battery_percentage} #{battery_remain}|"
+              # date and time
+              "%y/%m.%d(%u) %H:%M:%S"
+            ]}'
+            set -g status-right-length '80'
+          '';
+        }
+      ];
+      extraConfig = ''
+      '';
     };
+    home.packages = with pkgs; [
+      acpi # battery plugin
+    ];
   };
 }
