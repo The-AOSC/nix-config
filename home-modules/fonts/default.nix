960e44c17e5278114d499beae5a23d1e4bd48e56
The AOSC <the-aosc@tutamail.com>
2025-09-11 11:49:48 +0500

    Install nerd-fonts

--- home-modules/fonts/default.nix
+++ home-modules/fonts/default.nix
@@ -8,12 +8,14 @@
     modules.fonts.enable = lib.mkEnableOption "fonts";
   };
   config = lib.mkIf config.modules.fonts.enable {
-    home.packages = with pkgs; [
-      liberation_ttf
-      noto-fonts
-      source-code-pro
-      unifont
-    ];
+    home.packages = with pkgs;
+      [
+        liberation_ttf
+        noto-fonts
+        source-code-pro
+        unifont
+      ]
+      ++ (builtins.filter lib.attrsets.isDerivation (builtins.attrValues nerd-fonts));
     fonts.fontconfig = {
       enable = true;
       defaultFonts = {
