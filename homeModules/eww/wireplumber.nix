86ae6e46d2ac84d9056a1af4c489ac4e04d67085
The AOSC <the-aosc@tutamail.com>
2025-11-20 18:39:08 +0500

    eww: update wireplumber module

--- homeModules/eww/wireplumber.nix
+++ homeModules/eww/wireplumber.nix
@@ -3,12 +3,13 @@
     set -e -o pipefail
     sink="$(${pkgs.wireplumber}/bin/wpctl get-volume @DEFAULT_AUDIO_SINK@)"
     source="$(${pkgs.wireplumber}/bin/wpctl get-volume @DEFAULT_AUDIO_SOURCE@)"
-    printf '[%f,"%s",%f,"%s"]\n' "$(echo "$sink"|cut -d' ' -f2)" "$(echo "$sink"|cut -d' ' -f3)" "$(echo "$source"|cut -d' ' -f2)" "$(echo "$source"|cut -d' ' -f3)"
+    out="$(printf '[%f,"%s",%f,"%s"]' "$(echo "$sink"|cut -d' ' -f2)" "$(echo "$sink"|cut -d' ' -f3)" "$(echo "$source"|cut -d' ' -f2)" "$(echo "$source"|cut -d' ' -f3)")" || out="[]"
+    echo "$out"
   '';
 in {
   modules.eww.config = ''
     (defpoll wireplumber :interval "1s"
-                         :initial '[0,"[MUTED]",0,"[MUTED]"]'
+                         :initial '[]'
       "${wireplumber-info}")
   '';
 }
