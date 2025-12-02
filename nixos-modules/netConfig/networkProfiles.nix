2f265e37490288afc81e0d33b06fb807d78cb5cc
The AOSC <the-aosc@tutamail.com>
2025-12-02 20:59:09 +0500

    netConfig: don' create default connections

--- nixos-modules/netConfig/networkProfiles.nix
+++ nixos-modules/netConfig/networkProfiles.nix
@@ -27,6 +27,11 @@ in {
         }));
       default = {};
     };
+    createDefaultConnections = lib.mkOption {
+      description = "Create temporary wired connections for any Ethernet device that is managed and doesn't have a connection configured.";
+      type = lib.types.bool;
+      default = false;
+    };
   };
   config = lib.mkIf cfg.enable {
     sops.secrets = lib.mkMerge (lib.mapAttrsToList (_: network:
@@ -37,9 +42,14 @@ in {
       })
       network.secrets)
     cfg.networks);
-    networking.networkmanager.ensureProfiles = {
-      environmentFiles = lib.concatLists (lib.mapAttrsToList (_: network: lib.mapAttrsToList (name: _: config.sops.secrets."${name}".path) network.secrets) cfg.networks);
-      profiles = lib.mapAttrs (_: network: removeAttrs network ["secrets"]) cfg.networks;
+    networking.networkmanager = {
+      settings = lib.mkIf (!cfg.createDefaultConnections) {
+        main.no-auto-default = "*";
+      };
+      ensureProfiles = {
+        environmentFiles = lib.concatLists (lib.mapAttrsToList (_: network: lib.mapAttrsToList (name: _: config.sops.secrets."${name}".path) network.secrets) cfg.networks);
+        profiles = lib.mapAttrs (_: network: removeAttrs network ["secrets"]) cfg.networks;
+      };
     };
   };
 }
