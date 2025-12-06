3d14169e58542e7c741968ef8d62756ad0d50dfd
The AOSC <the-aosc@tutamail.com>
2025-12-07 00:07:15 +0500

    packages: update christbashtree

--- packages/christbashtree.nix
+++ packages/christbashtree.nix
@@ -1,6 +1,9 @@
 {
+  lib,
   stdenv,
   fetchFromGitHub,
+  makeWrapper,
+  ncurses,
   ...
 }:
 stdenv.mkDerivation {
@@ -11,10 +14,20 @@ stdenv.mkDerivation {
     rev = "bfea545af6d82f7b72f3ea1df816ee9262b2bc57";
     hash = "sha256-VSMGOM3cxWIZvIBIT41k28aPeJd/eeHn1WE53a+Hgh8=";
   };
+  nativeBuildInputs = [
+    makeWrapper
+  ];
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
     cp tree-EN.sh $out/bin/bash-tree
+    wrapProgram $out/bin/bash-tree \
+      --prefix PATH : ${lib.makeBinPath [ncurses]}
     runHook postInstall
   '';
+  meta = {
+    description = "Just a Christmas tree";
+    license = lib.licenses.unlicense;
+    mainProgram = "bash-tree";
+  };
 }
