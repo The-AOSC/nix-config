d5d5be4f330ab1d7e78757d1530ec93582cf33f5
The AOSC <the-aosc@tutamail.com>
2025-12-11 20:39:46 +0500

    uwsm: don't start on pts

--- home-modules/hyprland/default.nix
+++ home-modules/hyprland/default.nix
@@ -11,7 +11,7 @@ in {
   };
   config = lib.mkIf config.modules.hyprland.enable {
     programs.fish.loginShellInit = ''
-      if uwsm check may-start -g 0
+      if tty|not grep pts -q; and uwsm check may-start -g 0
         export UWSM_SILENT_START=2 # skip warning about graphical.target with 5 second wait
         exec uwsm start -g 0 ${config.wayland.windowManager.hyprland.finalPackage}/bin/Hyprland
       end
