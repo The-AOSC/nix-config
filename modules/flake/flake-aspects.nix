867e2cc95461b4a529a65bf1b07b5f06018bc1bf
The AOSC <the-aosc@tutamail.com>
2026-03-24 06:42:14 +0500

    dendritic: pull in home-manager configuration

--- modules/flake/flake-aspects.nix
+++ modules/flake/flake-aspects.nix
@@ -1,7 +1,36 @@
-{inputs, ...}: {
-  # https://flake-aspects.oeiuwq.com/guides/flake-parts/
+{
+  inputs,
+  lib,
+  ...
+}: {
   flake-file.inputs.flake-aspects.url = "github:vic/flake-aspects";
   imports = [
     inputs.flake-aspects.flakeModule
   ];
+  flake.aspects = let
+    aspects-lib = inputs.flake-aspects.lib lib;
+  in {
+    make-forward = {
+      each,
+      fromClass,
+      intoClass,
+      intoPath,
+      fromAspect,
+    } @ args:
+      aspects-lib.forward args;
+    make-once = {
+      key,
+      fromClasses,
+      fromAspect,
+    }: let
+      include = class: {
+        ${class} = {
+          key = "${builtins.toString key}-${class}";
+          imports = [(aspects-lib.resolve class [] fromAspect)];
+        };
+      };
+    in {
+      includes = map include fromClasses;
+    };
+  };
 }
--- modules/users/users.nix
+++ modules/users/users.nix
@@ -1,7 +1,11 @@
 {lib, ...}: {
   flake.aspects = {aspects, ...}: {
     users = username: {
-      includes = [(aspects.user._.${username} or {})];
+      includes = [
+        aspects.shared-home-manager
+        (aspects.user._.${username} or {})
+        (aspects.users username)._.home-manager
+      ];
       nixos.users.users.${username}.isNormalUser = lib.mkIf (username != "root") true;
       provides = {
         sops-password = sopsFile: {
@@ -14,7 +18,44 @@
             users.users.${username}.hashedPasswordFile = config.sops.secrets."${username}-password".path;
           };
         };
+        home-manager.includes = [
+          # user specific config
+          (aspects.make-forward {
+            each = [username];
+            fromClass = _: "homeManager";
+            intoClass = _: "nixos";
+            intoPath = user: ["home-manager" "users" user];
+            fromAspect = _: aspects.user._.${username} or {};
+          })
+        ];
       };
+    };
+    shared-home-manager = aspects.make-once {
+      # system specific config
+      key = lib.mapAttrsToList (n: v: "${n}-${builtins.toString v}") __curPos;
+      fromClasses = ["nixos"];
+      fromAspect.includes = [
+        {
+          nixos = {config, ...}: {
+            options.home-manager._sharedModule = lib.mkOption {
+              type = lib.types.raw;
+              internal = true;
+            };
+            config.home-manager.sharedModules = [config.home-manager._sharedModule];
+          };
+        }
+        ({
+          class,
+          aspect-chain,
+        }:
+          aspects.make-forward {
+            each = [null];
+            fromClass = _: "homeManager";
+            intoClass = _: "nixos";
+            intoPath = _: ["home-manager" "_sharedModule"];
+            fromAspect = _: lib.head aspect-chain;
+          })
+      ];
     };
     user.provides = {
       aosc = {
