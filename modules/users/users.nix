ee2b9afbeca597ec468dc14bbace8638785ca506
The AOSC <the-aosc@tutamail.com>
2026-03-24 21:01:34 +0500

    dendritic: embed shared home-manager config into per user config

--- modules/users/users.nix
+++ modules/users/users.nix
@@ -2,7 +2,6 @@
   flake.aspects = {aspects, ...}: {
     users = username: {
       includes = [
-        aspects.shared-home-manager
         (aspects.user._.${username} or {})
         (aspects.users username)._.home-manager
       ];
@@ -19,43 +18,25 @@
           };
         };
         home-manager.includes = [
-          # user specific config
-          (aspects.make-forward {
-            each = [username];
-            fromClass = _: "homeManager";
-            intoClass = _: "nixos";
-            intoPath = user: ["home-manager" "users" user];
-            fromAspect = _: aspects.user._.${username} or {};
-          })
+          # home-manager config
+          ({
+            class,
+            aspect-chain,
+          }:
+            aspects.make-forward {
+              each = [
+                # user specific config
+                (aspects.user._.${username} or {})
+                # system specific config
+                (lib.head aspect-chain)
+              ];
+              fromClass = _: "homeManager";
+              intoClass = _: "nixos";
+              intoPath = _: ["home-manager" "users" username];
+              fromAspect = aspect: aspect;
+            })
         ];
       };
-    };
-    shared-home-manager = aspects.make-once {
-      # system specific config
-      key = lib.mapAttrsToList (n: v: "${n}-${builtins.toString v}") __curPos;
-      fromClasses = ["nixos"];
-      fromAspect.includes = [
-        {
-          nixos = {config, ...}: {
-            options.home-manager._sharedModule = lib.mkOption {
-              type = lib.types.raw;
-              internal = true;
-            };
-            config.home-manager.sharedModules = [config.home-manager._sharedModule];
-          };
-        }
-        ({
-          class,
-          aspect-chain,
-        }:
-          aspects.make-forward {
-            each = [null];
-            fromClass = _: "homeManager";
-            intoClass = _: "nixos";
-            intoPath = _: ["home-manager" "_sharedModule"];
-            fromAspect = _: lib.head aspect-chain;
-          })
-      ];
     };
     user.provides = {
       aosc = {
