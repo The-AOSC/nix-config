b9027f360b5f892e5044f96dc4c9eea228386f15
The AOSC <the-aosc@tutamail.com>
2026-04-21 15:36:59 +0500

    boot: fix enableNumlock service

--- nixos-modules/enableNumlock/default.nix
+++ nixos-modules/enableNumlock/default.nix
@@ -10,20 +10,25 @@
   config = lib.mkIf config.modules.enableNumlock.enable {
     boot.initrd.preLVMCommands = lib.mkIf (!config.boot.initrd.systemd.enable) ''
       for tty in ${lib.concatMapStringsSep " " (n: "/dev/tty${builtins.toString n}") (lib.range 1 63)}; do
-        ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+        ${pkgs.kbd}/bin/setleds -D +num < "$tty"
       done
     '';
-    boot.initrd.systemd.services.enableNumlock = {
-      before = ["cryptsetup.target"];
-      wantedBy = ["initrd.target"];
-      script = ''
-        for tty in ${lib.concatMapStringsSep " " (n: "/dev/tty${builtins.toString n}") (lib.range 1 63)}; do
-          ${pkgs.kbd}/bin/setleds -D +num < "$tty";
-        done
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
+    boot.initrd.systemd = {
+      storePaths = [
+        "${config.boot.initrd.systemd.package.kbd}/bin/setleds"
+      ];
+      services.enableNumlock = {
+        before = ["cryptsetup.target"];
+        wantedBy = ["initrd.target"];
+        script = ''
+          for tty in ${lib.concatMapStringsSep " " (n: "/dev/tty${builtins.toString n}") (lib.range 1 63)}; do
+            ${pkgs.kbd}/bin/setleds -D +num < "$tty"
+          done
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
       };
     };
   };
