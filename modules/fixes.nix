16f496d9228c53bee75cfd66d0bcdf6a1f846898
The AOSC <the-aosc@tutamail.com>
2026-05-15 22:42:12 +0500

    wine: fix wine-staging. AGAIN

--- modules/fixes.nix
+++ modules/fixes.nix
@@ -20,6 +20,54 @@
                 });
           };
       });
+      wine-ge-fixed = final.wine-ge.overrideAttrs (finalAttrs: old: {
+        patches =
+          old.patches or []
+          ++ [
+            # See https://gitlab.winehq.org/wine/wine/-/merge_requests/7328
+            (final.fetchpatch2 {
+              url = "https://gitlab.winehq.org/wine/wine/-/commit/c9519f68ea04915a60704534ab3afec5ec1b8fd7.patch";
+              hash = "sha256-b36pa0EdJnOeuZ8+21QfS30WMSEHKTPQpnXsTvmtw30=";
+            })
+            (final.fetchpatch2 {
+              url = "https://gitlab.winehq.org/wine/wine/-/commit/fd59962827a715d321f91c9bdb43f3e61f9ebbcb.patch";
+              hash = "sha256-ssPEdzjE+R4KbLFrasf279bX++bhzC+K/LXxhAL5liI=";
+            })
+          ];
+        postInstall = let
+          wine-mono = final.fetchurl rec {
+            # https://gitlab.winehq.org/wine/wine/-/wikis/Wine-Mono#versions
+            hash = "sha256-DtPsUzrvebLzEhVZMc97EIAAmsDFtMK8/rZ4rJSOCBA=";
+            version = "wine-mono-8.1.0";
+            url = "https://github.com/wine-mono/wine-mono/releases/download/${version}/${version}-x86.msi";
+          };
+        in ''
+          ${old.postInstall or ""}
+          ln -s ${wine-mono} $out/share/wine/mono/${wine-mono.name}
+        '';
+      });
+      wine-staging-fixed = let
+        wine = final.wineWow64Packages.stagingFull;
+        wine-mono = final.fetchurl rec {
+          # https://gitlab.winehq.org/wine/wine/-/wikis/Wine-Mono#versions
+          version = "11.1.0";
+          url = "https://dl.winehq.org/wine/wine-mono/${version}/wine-mono-${version}-x86.msi";
+          hash = "sha256-3rA0FDH4Jgsgn/9rx53cxUFLl/jpI2q5+9ykzlngqbk=";
+        };
+      in
+        final.runCommand wine.name {
+          inherit (wine) meta;
+          passthru =
+            wine.passthru
+            // {
+              inherit wine;
+            };
+        } ''
+          cp ${wine} $out -ra
+          chmod +200 $out -R
+          find $out -type f | xargs sed -i "s#$(sed "s#/nix/store/\([0-9a-z]*\)-.*#\1#" <<< "${wine.outPath}")#$(sed "s#/nix/store/\([0-9a-z]*\)-.*#\1#" <<< "$out")#g"
+          ln -s ${wine-mono} $out/share/wine/mono/${wine-mono.name}
+        '';
       wireshark = prev.wireshark.overrideAttrs (old: {
         patches =
           old.patches or []
