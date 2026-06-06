38132ab6272dd826f89784d9eaf108582bae0ba9
The AOSC <the-aosc@tutamail.com>
2026-06-06 08:55:18 +0500

    glide: fix evaluation warning

--- modules/misc/glide.nix
+++ modules/misc/glide.nix
@@ -11,43 +11,48 @@
       ...
     }: {
       imports = [inputs.glide.nixosModules.default];
-      services.kanata.keyboards.${instance}.port = 7070;
-      modules.kanata.keyboards.${instance}.extraConfig = ''
-        (defvirtualkeys
-          pad-touch (switch
-                      ((base-layer default)) (layer-while-held default-mouse) break
-                      ((base-layer simple)) (layer-while-held mouse) break))
-      '';
-      services.glide = {
-        enable = true;
-        # drop --device option
-        package = pkgs.writeCBin "glide" ''
-          #include <string.h>
-          #include <unistd.h>
-          int main(int argc, char **argv) {
-            if (argc<1) {
-              return 1;
-            }
-            char **arg = argv;
-            while (*++arg) {
-              if (!strcmp(*arg, "--device")) {
-                break;
-              }
-            }
-            if (*(arg+1)) {
-              while (*(arg+2)) {
-                *arg = *(arg+2);
-                arg++;
-              }
-            }
-            *arg = NULL;
-            argv[0] = "glide";
-            execve("${lib.getExe inputs.glide.packages.${pkgs.stdenv.hostPlatform.system}.default}", argv, NULL);
-          }
+      options.services.glide.package = lib.mkOption {
+        defaultText = ''inputs.glide.packages.''${pkgs.system}.default'';
+      };
+      config = {
+        services.kanata.keyboards.${instance}.port = 7070;
+        modules.kanata.keyboards.${instance}.extraConfig = ''
+          (defvirtualkeys
+            pad-touch (switch
+                        ((base-layer default)) (layer-while-held default-mouse) break
+                        ((base-layer simple)) (layer-while-held mouse) break))
         '';
-        # activate immediately on touch
-        motionThreshold = 0;
-        minStreak = 0;
+        services.glide = {
+          enable = true;
+          # drop --device option
+          package = pkgs.writeCBin "glide" ''
+            #include <string.h>
+            #include <unistd.h>
+            int main(int argc, char **argv) {
+              if (argc<1) {
+                return 1;
+              }
+              char **arg = argv;
+              while (*++arg) {
+                if (!strcmp(*arg, "--device")) {
+                  break;
+                }
+              }
+              if (*(arg+1)) {
+                while (*(arg+2)) {
+                  *arg = *(arg+2);
+                  arg++;
+                }
+              }
+              *arg = NULL;
+              argv[0] = "glide";
+              execve("${lib.getExe' inputs.glide.packages.${pkgs.stdenv.hostPlatform.system}.default "glide"}", argv, NULL);
+            }
+          '';
+          # activate immediately on touch
+          motionThreshold = 0;
+          minStreak = 0;
+        };
       };
     };
   };
